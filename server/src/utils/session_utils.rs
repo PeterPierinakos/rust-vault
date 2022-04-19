5616b28ec3c23a223daa824d40954e7711b772fd
PeterPierinakos <qLuc4rM39siZdi78WkvoPd0h79eYCjNEQ2fS0Ht@protonmail.com>
2022-04-19 18:57:58 +0300

    Remove to rename

--- server/src/utils/session_utils.rs
+++
@@ -1,9 +0,0 @@
-use actix_session::Session;
-
-pub fn verify_session(session: &Session) -> bool {
-    if let Some(_id) = session.get::<i32>("id").unwrap() {
-        return true;
-    } else {
-        return false;
-    }
-}
